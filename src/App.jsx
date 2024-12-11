b3266106b7ba4ba7b9702495829af7d6983cf59e
ChiThuan <tranchithuanjr@gmail.com>
2024-12-11 15:59:27 +0700

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import ListPatient from "./Dashboard/ListPatient";
+import Patient from "./Dashboard/Patient";
+import Medicine from "./Dashboard/Medicine";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route path="/listpatient" element={<ListPatient />} />
+          <Route path="/patient" element={<Patient />} />
+          <Route path="/medicine" element={<Medicine />} />
+        </Route>
       </Routes>
     </>
   );
