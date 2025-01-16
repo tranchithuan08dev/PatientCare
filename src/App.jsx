b1bfb81d4cafc538cbe795aa4a63b93afe4ac1d8
ChiThuan <tranchithuanjr@gmail.com>
2025-01-16 18:21:25 +0700

    add router list patient and revenue

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Dashboard from "./Dashboard";
 import ListPatient from "./Dashboard/ListPatient";
 import Patient from "./Dashboard/Patient";
 import Medicine from "./Dashboard/Medicine";
+import ListMedicine from "./Dashboard/ListMedicine";
+import Revenue from "./Dashboard/Revenue";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Dashboard />}>
+          <Route path="/revenue" element={<Revenue />} />
           <Route path="/listpatient" element={<ListPatient />} />
           <Route path="/patient" element={<Patient />} />
+          <Route path="/listmedicine" element={<ListMedicine />} />
           <Route path="/medicine" element={<Medicine />} />
         </Route>
       </Routes>
