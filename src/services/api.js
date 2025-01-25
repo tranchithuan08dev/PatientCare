037034a260ca040b2a6344317393e819a855e25f
ChiThuan <tranchithuanjr@gmail.com>
2025-01-25 22:28:32 +0700

    change API

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = {
   call: () => {
     return axios.create({
-      baseURL: `http://localhost:5000`,
+      baseURL: `http://localhost:5000/api/`,
     });
   },
 };
