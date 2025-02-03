5d1a5fb384f32942995ad3ea8a65122191856d1b
ChiThuan <tranchithuanjr@gmail.com>
2025-02-03 22:42:23 +0700

    create user slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import medicine from "./medicineSlice";
-
+import userSlice from "./userSlice";
 const store = configureStore({
   reducer: {
     MEDICINE: medicine,
+    USER: userSlice,
   },
 });
 
