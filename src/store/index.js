24c2c3ee95e2cc30e82141aa6a226eca3f3fa579
ChiThuan <tranchithuanjr@gmail.com>
2025-02-05 15:33:06 +0700

    create DIAGNOSIS reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import medicine from "./medicineSlice";
 import userSlice from "./userSlice";
+import diagnosisSlice from "./diagnosisSlice";
 const store = configureStore({
   reducer: {
     MEDICINE: medicine,
     USER: userSlice,
+    DIAGNOSIS: diagnosisSlice,
   },
 });
 
