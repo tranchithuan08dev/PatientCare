dcc87e75eff695bad3cc1b810b6a5eaada5a467f
ChiThuan <tranchithuanjr@gmail.com>
2025-02-05 19:25:28 +0700

    create getAll

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,9 @@ const userServices = {
   create: (data) => {
     return API.call().post(`/user/create`, data);
   },
+  getAll: () => {
+    return API.call().get(`/user/getAll`);
+  },
 };
 
 export default userServices;
