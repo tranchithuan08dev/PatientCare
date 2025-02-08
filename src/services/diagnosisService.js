270f850de152a8d63a9b79d56c2d4f6aaa3c89f9
ChiThuan <tranchithuanjr@gmail.com>
2025-02-08 17:14:16 +0700

    create get detail medicine of diagnosis

--- src/services/diagnosisService.js
+++ src/services/diagnosisService.js
@@ -7,6 +7,9 @@ const diagnosisService = {
   getDetail: (id) => {
     return API.call().get(`/diagnosis/getdetail?id=${id}`);
   },
+  getDetailMedicine: (id) => {
+    return API.call().get(`/diagnosis/getdetailmedicine?id=${id}`);
+  },
 };
 
 export default diagnosisService;
