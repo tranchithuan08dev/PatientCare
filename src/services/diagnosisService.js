ef18ac7d7915280dc9422d5b874d5428311395cf
ChiThuan <tranchithuanjr@gmail.com>
2025-02-08 16:15:28 +0700

    create getdetail diagnosis

--- src/services/diagnosisService.js
+++ src/services/diagnosisService.js
@@ -4,6 +4,9 @@ const diagnosisService = {
   create: (data) => {
     return API.call().post("/diagnosis/create", data);
   },
+  getDetail: (id) => {
+    return API.call().get(`/diagnosis/getdetail?id=${id}`);
+  },
 };
 
 export default diagnosisService;
