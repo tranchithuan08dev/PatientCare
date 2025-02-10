ae975952e0012dd7335dd3b05ec10af5b18e9dca
ChiThuan <tranchithuanjr@gmail.com>
2025-02-10 14:08:47 +0700

    create reducer payment

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import medicine from "./medicineSlice";
 import userSlice from "./userSlice";
 import diagnosisSlice from "./diagnosisSlice";
+import paymentSlice from "./paymentSlice";
 const store = configureStore({
   reducer: {
     MEDICINE: medicine,
     USER: userSlice,
     DIAGNOSIS: diagnosisSlice,
+    PAYMENT: paymentSlice,
   },
 });
 
